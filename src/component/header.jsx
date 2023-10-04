c503994e683194af12c8850062d1acd887336831
Jishan <86280904+ABDUL-AZIZ-JISHAN@users.noreply.github.com>
2023-10-04 20:59:38 +0600

    ready to deploy

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header id="header">
       <div className="container">
-        <img src="./img/lws-logo.svg" alt="logo" className="logo" />
+        <h2 style={{color: "white", fontSize: "30px", fontWeight:"bold"}}>Redux Flight Booking App</h2>
         <div className="flex items-center">
           <a className="text-white min-w-[50px] font-medium" href="#">
             Home
